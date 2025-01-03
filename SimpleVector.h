868cbc0efa01bf02b9c2b399d1ff4a6eb682f8ce
skeldonky <75779367+skeldonky@users.noreply.github.com>
2025-01-03 14:57:44 +0900

    주석삭제

--- SimpleVector.h
+++ SimpleVector.h
@@ -1,36 +1,3 @@
-//#pragma once
-//#include <iostream>
-//#include <vector>
-//
-//template <typename T>
-//class SimpleVector
-//{
-//protected:
-//	T* data;//동적배열
-//	int currentSize;//현재 배열의 크기
-//	int currentCapacity;//현재 배열의 최대 크기
-//
-//public:
-//	//생성자
-//	SimpleVector();
-//	SimpleVector(int capacity);
-//	//정수가 현재배열의 크기보다 작으면 동작X, 만약 배열의 크기보다 크면 해당 값 만큼 크기를 재 할당.
-//	void resize();
-//	//배열 끝에 추가 (꽉찼으면 동작X)
-//	void push_back(const T& value);
-//	//배열 마지막 원소 제거 (제거 할 원소가 없으면 동작X)
-//	void pop_back();
-//	//현재 원소의 개수
-//	int size();
-//	//배열의 크기
-//	int capacity();
-//
-//	//소멸자
-//	~SimpleVector();
-//};
-
-
-
 #ifndef SIMPLEVECTOR_H
 #define SIMPLEVECTOR_H
 
@@ -38,9 +5,9 @@ template <typename T>
 class SimpleVector
 {
 protected:
-	T* data;//동적배열
-	int currentSize;//현재 배열의 크기
-	int currentCapacity;//현재 배열의 최대 크기
+	T* data;				//동적배열
+	int currentSize;		//현재 배열의 크기
+	int currentCapacity;	//현재 최대 크기
 
 public:
 	//생성자
